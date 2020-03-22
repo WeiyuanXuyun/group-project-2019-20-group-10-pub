344a4541fb028e525951451ed6273f3c80af2229
Ryan <Ryan.Davatwal@outlook.com>
2020-03-22 13:30:12 +0000

    Asking question (No Randomness)

--- app/src/main/java/com/example/cs2001_group10/questions.java
+++ app/src/main/java/com/example/cs2001_group10/questions.java
@@ -3,9 +3,13 @@ package com.example.cs2001_group10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -23,35 +27,80 @@ import java.util.Map;
 
 public class questions extends AppCompatActivity {
     private static final String TAG = "Questions Screen Activity" ;
-    ArrayList<String> maths_list = new ArrayList<>();
-    private TextView answer_one, answer_two, correct_answer;
+    ArrayList<String> maths_Questions = new ArrayList<>();
+    private TextView question;
     private String ans_result, ans_two_result, correct_ans_result;
-
+    private Button b_ans1, b_ans2, b_ans3;
+    private Boolean first_Time =true;
+    private int q_Num, current_Question = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
 
         Log.d(TAG, "onCreate: " + home_screen.Maths_List);
+        b_ans1 = findViewById(R.id.b_A1);
+        b_ans2 = findViewById(R.id.b_A2);
+        b_ans3 = findViewById(R.id.b_A3);
+        question = findViewById(R.id.t_Question);
 
 
-        int Topic = 1;
 
-        if (Topic == 1) {
-            String Question = Random_Question(home_screen.Maths_List);
-            Log.d(TAG, "Topic Question: " + Question);
-            GetAnswers(Question, Api.URL_MATHS_ANSWERS_REQUEST, "maths_questions");
+        if (first_Time == true){
+            Log.d(TAG, "onCreate: boolean check");
+            for (int i = 0; i <5; i++){
+                q_Num = i; //Add random num 5 <-
+                maths_Questions.add(Random_Question(home_screen.Maths_List, q_Num));
+            }
+            first_Time = false;
+        } else if (current_Question == 4){
+            first_Time = true;
+            current_Question = 0;
         }
 
+            Log.d(TAG, "Topic Question: " + maths_Questions);
+
+
+            question.setText(home_screen.Maths_List.get(current_Question));
+            GetAnswers(maths_Questions.get(current_Question), Api.URL_MATHS_ANSWERS_REQUEST, "maths_questions");
+            current_Question++;
+
+        b_ans1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(questions.this, "incorrect", Toast.LENGTH_SHORT).show();
+                next_Question();
+            }
+        });
+
+        b_ans2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(questions.this, "incorrect", Toast.LENGTH_SHORT).show();
+                next_Question();
+            }
+        });
+
+        b_ans3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(questions.this, "correct", Toast.LENGTH_SHORT).show();
+                next_Question();
+            }
+        });
     }
 
+    public void next_Question() {
+        question.setText(home_screen.Maths_List.get(current_Question));
+        GetAnswers(maths_Questions.get(current_Question), Api.URL_MATHS_ANSWERS_REQUEST, "maths_questions");
+        current_Question++;
+    }
 
     // method that gets random question
 
-    private String Random_Question(ArrayList<String> maths_List) {
-        //Log.d(TAG, "Random_Question: " + home_screen.Maths_List.get(1));
-
-        return  home_screen.Maths_List.get(1);
+    private String Random_Question(ArrayList<String> maths_List, int q_Num) {
+        Log.d(TAG, "Random_Question: " + home_screen.Maths_List.get(q_Num));
+        return  home_screen.Maths_List.get(q_Num);
     }
 
     private void GetAnswers(final String question, String URL, final String Array_Name) {
@@ -66,9 +115,11 @@ public class questions extends AppCompatActivity {
                             Log.d(TAG, "onResponse: " + jsonObject);
                             JSONArray Array = jsonObject.getJSONArray(Array_Name);
 
-
+                            b_ans1.setText("First Answer: " + Array.getJSONObject(0).getString("answer"));
                             ans_result = Array.getJSONObject(0).getString("answer");
+                            b_ans2.setText("Second Answer: " + Array.getJSONObject(0).getString("answer_2"));
                             ans_two_result = Array.getJSONObject(0).getString("answer_2");
+                            b_ans3.setText("Third Answer: " + Array.getJSONObject(0).getString("correct_answer"));
                             correct_ans_result = Array.getJSONObject(0).getString("correct_answer");
 
                             Log.d(TAG, "onResponse: answers" + ans_result + " " + ans_two_result + " " + correct_ans_result);
@@ -97,8 +148,5 @@ public class questions extends AppCompatActivity {
         requestQueue.add(stringRequest);
 
     }
-
-
-
 
 }
